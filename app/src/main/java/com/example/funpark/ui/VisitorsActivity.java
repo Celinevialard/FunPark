c67732ecac3975d752c0697433ef42988694ab15
celine.vialard2 <celine.vialard@students.hevs.ch>
2022-11-12 12:19:56 +0100

    add base activity, add visitor list

---
+++ app/src/main/java/com/example/funpark/ui/VisitorsActivity.java
@@ -0,0 +1,165 @@
+package com.example.funpark.ui;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.lifecycle.ViewModelStore;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.funpark.R;
+import com.example.funpark.adapter.RecyclerAdapter;
+import com.example.funpark.database.entity.TicketEntity;
+import com.example.funpark.database.entity.VisitorEntity;
+import com.example.funpark.util.OnAsyncEventListener;
+import com.example.funpark.util.RecyclerViewItemClickListener;
+import com.example.funpark.viewmodel.visitor.VisitorListViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitorsActivity extends BaseActivity {
+
+    private static final String TAG = "AccountsActivity";
+
+    private List<VisitorEntity> visitors;
+    private RecyclerAdapter<VisitorEntity> adapter;
+    private VisitorListViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLayoutInflater().inflate(R.layout.activity_visitors, frameLayout);
+
+        setTitle(getString(R.string.title_activity_visitor));
+        navigationView.setCheckedItem(tabulation);
+
+        RecyclerView recyclerView = findViewById(R.id.visitorRecyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        //mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        // TODO
+        //SharedPreferences settings = getSharedPreferences(BaseActivity.PREFS_NAME, 0);
+        //String user = settings.getString(BaseActivity.PREFS_USER, null);
+
+        visitors = new ArrayList<>();
+        adapter = new RecyclerAdapter<>(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Log.d(TAG, "clicked position:" + position);
+                Log.d(TAG, "clicked on: " + visitors.get(position).getFirstName());
+
+                /*
+                Intent intent = new Intent(VisitorsActivity.this,VisitorDetailActivity.class);
+                intent.setFlags(
+                        Intent.FLAG_ACTIVITY_NO_ANIMATION |
+                                Intent.FLAG_ACTIVITY_NO_HISTORY
+                );
+                intent.putExtra("accountId", visitors.get(position).getId());
+                startActivity(intent);
+                 */
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+                Log.d(TAG, "longClicked position:" + position);
+                Log.d(TAG, "longClicked on: " + visitors.get(position).getFirstName());
+
+                createDeleteDialog(position);
+            }
+        });
+
+        /*
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(view -> {
+                    Intent intent = new Intent(VisitorsActivity.this, VisitorsEditActivity.class);
+                    intent.setFlags(
+                            Intent.FLAG_ACTIVITY_NO_ANIMATION |
+                                    Intent.FLAG_ACTIVITY_NO_HISTORY
+                    );
+                    startActivity(intent);
+                }
+        );
+*/
+        VisitorListViewModel.Factory factory = new VisitorListViewModel.Factory(
+                getApplication());
+        viewModel = new ViewModelProvider(new ViewModelStore(),(ViewModelProvider.Factory) factory).get(VisitorListViewModel.class);
+        viewModel.getVisitors().observe(this, accountEntities -> {
+            if (accountEntities != null) {
+                visitors = accountEntities;
+                adapter.setData(visitors);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == BaseActivity.tabulation) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return false;
+        }
+        /*
+        The activity has to be finished manually in order to guarantee the navigation hierarchy working.
+        */
+        finish();
+        return super.onNavigationItemSelected(item);
+    }
+
+    private void createDeleteDialog(final int position) {
+        final VisitorEntity visitor = visitors.get(position);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        final View view = inflater.inflate(R.layout.row_delete_item, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(getString(R.string.title_activity_delete_visitor));
+        alertDialog.setCancelable(false);
+
+        final TextView deleteMessage = view.findViewById(R.id.tv_delete_item);
+        deleteMessage.setText(String.format(getString(R.string.visitor_delete_msg), visitor.getFirstName()));
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.action_accept), (dialog, which) -> {
+            Toast toast = Toast.makeText(this, getString(R.string.visitor_deleted), Toast.LENGTH_LONG);
+            viewModel.deleteAccount(visitor, new OnAsyncEventListener() {
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "deleteAccount: success");
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    Log.d(TAG, "deleteAccount: failure", e);
+                }
+            });
+            toast.show();
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.action_cancel), (dialog, which) -> alertDialog.dismiss());
+        alertDialog.setView(view);
+        alertDialog.show();
+    }
+}
